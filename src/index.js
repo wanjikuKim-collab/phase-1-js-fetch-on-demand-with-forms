4ba5f5ce2dad58fdbf0db11c89afa8eb297070f6
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-04 13:17:13 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+// we want The JS code we write to execute after the DOM is fully loaded
+// to do this, our codes can either be in `init` or  we call a function within init
 const init = () => {
-  
+    //1.Add event listeners to capture form data and override a form's default behavior
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit',(e)=>{       
+        e.preventDefault(); 
+
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value)
+        //2.Access Input Value from an Event Object
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp=> resp.json()) 
+        .then(data=>{
+            console.log(data)
+            const title = document.querySelector('h4');
+            const summary = document.querySelector('p');
+
+            title.innerText = data.title;
+            summary.innerText= data.summary;
+        
+        })
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
